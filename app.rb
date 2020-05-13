266ee32c146755365022268bb14b2013ed947fca
gojoaromokudu <gojoaromokudu@gmail.com>
2020-05-13 22:45:25 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
+# require "pry"
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    # binding.pry
+    erb :index
+  end
 
+  post '/greet' do
+    erb :greet
+
+  end
 end
